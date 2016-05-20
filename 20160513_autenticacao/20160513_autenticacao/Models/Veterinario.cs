40a23c5f07ef0a8d58369ccef6ff452650b1166e
pedroHdias <diaspedro1999@hotmail.com>
2016-05-20 11:22:01 +0100

    classes com os atributos das entidades a serem criadas na DB

---
+++ 20160513_autenticacao/20160513_autenticacao/Models/Veterinario.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace _20160513_autenticacao.Models
+{
+    public class Veterinario
+    {
+        [Key]
+        public int VeterinarioID { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(30, MinimumLength = 3)]
+        public string Nome { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(50, MinimumLength = 5)]
+        public string Rua { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(3, MinimumLength = 1)]
+        public string NumPorta { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(3, MinimumLength = 1)]
+        public string Andar { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(30)]
+        public string CodPostal { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(9, ErrorMessage = "O {0} tem de ter {2} caracteres.", MinimumLength = 9)]
+        public string NIF { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime DataEntradaClinica { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(30, MinimumLength = 10)]
+        public string NumCedulaProf { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime DataInscOrdem { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(50, MinimumLength = 3)]
+        public string Faculdade { get; set; }
+    }
+}
