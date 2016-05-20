9b973199dbdf8a60e82b35ae7fb8fede42145e12
pedroHdias <diaspedro1999@hotmail.com>
2016-05-20 11:29:53 +0100

    classes com os atributos das entidades a serem criadas na DB

---
+++ 20160513_autenticacao/20160513_autenticacao/Models/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _20160513_autenticacao.Models
+{
+    public class Animal
+    {
+        [Key]
+        public int AnimalID { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(30)]
+        public string Nome { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(30)]
+        public string Especie { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(30)]
+        public string Raca { get; set; }
+
+        [DataType(DataType.Text)]
+        public float Peso { get; set; }
+
+        // ? => é de preenchimento facultativo
+        [DataType(DataType.Text)]
+        public float? Altura { get; set; }
+    }
+}
---
+++ 20160513_autenticacao/20160513_autenticacao/Models/Dono.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _20160513_autenticacao.Models
+{
+    public class Dono
+    {
+        [Key]
+        public int DonoId { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(30)]
+        public string Nome { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(9, ErrorMessage = "O {0} tem de ter {2} caracteres.", MinimumLength = 9)]
+        public string NIF { get; set; }
+    }
+}
